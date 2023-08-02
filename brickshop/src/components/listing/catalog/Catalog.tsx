6fc025252143ab1b92cb3106b6d1032180d30fc3
Rafael Solano <nemesysolano@gmail.com>
2023-08-02 14:57:31 -0400

    Added icons to product info card

--- brickshop/src/components/listing/catalog/Catalog.tsx
+++ brickshop/src/components/listing/catalog/Catalog.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonItem, IonLabel, IonList } from '@ionic/react';
 import { Product } from '../../../model';
 import "./Catalog.css"
 import { useCatalogContext } from '../../../context';
+import { ProductInfo } from '../../ProductInfo';
 
 export interface CatalogProperties {
     products: Product[]
 }
 
-const CatalogItem: React.FC<Product> = (product: Product) => (
-  <IonItem key={product.id}>
-    <img src={product.picture} className='image'></img>
-    <IonLabel>{product.name}</IonLabel>
+const CatalogItem: React.FC<{product: Product, onClick: () => void}> = (props:{product: Product, onClick: () => void}) => (
+  <IonItem className="item" onClick={props.onClick}>
+    <img src={props.product.picture} className='image'></img>
+    <IonLabel>{props.product.name}</IonLabel>
   </IonItem>
 );
   
@@ -19,10 +20,18 @@ const CatalogItem: React.FC<Product> = (product: Product) => (
 
 export const Catalog: React.FC<CatalogProperties> = () => {
     const catalogContext = useCatalogContext();
-
+    const [isOpen, setIsOpen] = useState(false);
+    const [product, setProduct] = useState<Product>({} as Product)
+    const onItemClick = (product: Product) => {
+      setProduct(product);
+      setIsOpen(true);
+    }
     return (
-      <IonList inset={true} className='catalog'>
-        {catalogContext.products.map(CatalogItem)}        
-      </IonList>
+      <>
+        <IonList inset={true} className='catalog'>
+          {catalogContext.products.map(item => <CatalogItem key={item.id} product={item} onClick={() => onItemClick(item)}/>)}        
+        </IonList>
+        <ProductInfo product={product} isOpen={isOpen}/>
+      </>
     )
 }
--- brickshop/src/pages/Home.tsx
+++ brickshop/src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Product Catalog</IonTitle>
+          <IonTitle className='ion-text-center'>Product Catalog</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
